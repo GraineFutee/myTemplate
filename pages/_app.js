99792c3e9d96f1f1b92f34ff619c35de10313eff
Pierre ASDRUBAL <40754894+GraineFutee@users.noreply.github.com>
2020-11-03 16:32:35 +0100

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import "@fortawesome/fontawesome-free/css/all.css";
+import "../styles/css/main.css";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
